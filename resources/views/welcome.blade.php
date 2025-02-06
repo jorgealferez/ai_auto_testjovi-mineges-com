d72678616054f2e4543efe87a407c2402bed3631
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-06 16:59:35 +0100

    Actualización automática con IA

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenido a Laravel</h1>
+        <p>Esto lo hemos generado en una rama secundaria número 4</p>
+    </div>
+</body>
+</html>
