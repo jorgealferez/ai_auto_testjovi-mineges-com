c9914f606dead5479cf25eb921b7ad8085645fff
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-06 16:21:25 +0100

    Actualización automática con IA

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Bienvenido a nuestra aplicación</h1>
+    <p>Hemos modificado el fichero seleccionándolo en el buscador.</p>
+</body>
+</html>
