1dea78044da5e329cc92bc75de06347fcbac3f97
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-06 14:49:06 +0100

    Actualización automática con IA

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test de Jovi</title>
-</head>
-<body>
-    <h1>Este es el test de Jovi</h1>
-</body>
-</html>
+{
+  "html": {
+    "lang": "es",
+    "head": {
+      "meta": [
+        {
+          "charset": "UTF-8"
+        },
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1.0"
+        }
+      ],
+      "title": "Test de Jovi"
+    },
+    "body": {
+      "h1": "Este es el test de Jovi"
+    }
+  }
+}
