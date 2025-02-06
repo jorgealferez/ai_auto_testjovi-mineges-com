f016ec19498c8eb887f8b50de62c46a3440b0e8d
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-06 17:00:33 +0100

    Actualización automática con IA

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>Welcome</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <h1>Welcome to Our Website</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque, nunc at sodales suscipit, dolor massa vehicula lorem, vel gravida metus elit a diam. Proin aliquam libero at urna finibus, a hendrerit augue cursus. Pellentesque non eros et nibh fermentum tincidunt. In facilisis odio vel massa cursus, a viverra est blandit. Mauris interdum maximus nisi, ut malesuada eros condimentum ut. Curabitur vel est eget est finibus feugiat. Vestibulum feugiat, leo vel ultrices dignissim, justo turpis fermentum nulla, ut iaculis orci libero a odio. Ut at ligula ac tortor ultrices volutpat. Aenean vel mattis urna. Quisque efficitur dolor ut lorem vestibulum, non cursus nunc sodales.
+
+        Suspendisse consectetur porttitor metus vel consequat. Cras hendrerit id velit et sodales. Aliquam vel tempus purus, a lobortis sapien. Phasellus nec mi ac neque tincidunt vulputate. Donec ultrices vitae mauris ac dictum. Mauris dictum odio sit amet justo bibendum, non fermentum ligula convallis. Vivamus lobortis, felis at pharetra tincidunt, est libero efficitur lacus, non dapibus lorem massa eget metus. Morbi molestie consequat sapien, ac sodales odio viverra a. Fusce aliquam eu dui at consectetur. Etiam euismod volutpat nisl vitae auctor. Ut non risus non velit mattis lobortis vel et arcu. Nulla sit amet lorem mi. Morbi non purus vel eros scelerisque tincidunt. Nulla felis magna, feugiat ac malesuada in, fermentum eu justo.
+
+        Cras mollis ligula id purus cursus aliquet. Phasellus aliquet ligula a pharetra hendrerit. Integer bibendum interdum vestibulum. Vestibulum eget mauris ut odio hendrerit tincidunt. Duis ut metus accumsan, tempor ligula in, laoreet augue. Etiam fermentum orci nec ultricies facilisis. Sed at volutpat sem. Sed porttitor magna eu arcu dignissim, in finibus nunc aliquet. Praesent volutpat arcu vitae nisl congue, at efficitur velit vulputate. Sed ut lacus tempor, vestibulum elit nec, bibendum nunc. Praesent et volutpat purus. Fusce et arcu id arcu sollicitudin commodo.
+
+        Nulla a volutpat dui, id vehicula magna. Duis eget elit sit amet enim venenatis lobortis. Vivamus ac vestibulum neque. Suspendisse non sagittis massa. Vestibulum tincidunt sapien a scelerisque lobortis. Sed sed lacinia arcu, sed varius nulla. Maecenas et turpis a erat commodo luctus. Vestibulum sed augue id justo consectetur facilisis. Sed rutrum arcu nec aliquam auctor. Praesent finibus nec velit nec fermentum. Vivamus consectetur nulla sit amet lorem vehicula, nec dignissim libero bibendum. Cras sollicitudin tellus erat, quis posuere nunc aliquet vel. Fusce non nisi in odio sollicitudin sollicitudin.
+
+        Aenean pretium bibendum tellus ut convallis. Mauris accumsan malesuada sollicitudin. Suspendisse potenti. Cras lobortis libero arcu, ac ultrices risus vehicula sit amet. Proin id purus sed tellus congue feugiat. Donec non pellentesque diam. Donec ultricies, magna nec scelerisque fermentum, nisi magna venenatis libero, a cursus nunc arcu ac nulla. Ut laoreet, justo nec tempor interdum, ipsum velit hendrerit justo, quis tempor urna metus a felis. Integer cursus sem nec quam sodales vehicula. Donec a nisl fermentum magna vestibulum scelerisque. Maecenas commodo, est vitae sodales vehicula, libero orci efficitur neque, non efficitur est elit vel ante.
+    </p>
+    </div>
+
+    <footer class="bg-light text-center py-4">
+        <p>© 2023 Your Website. All rights reserved.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
