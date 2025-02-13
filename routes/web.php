7cdceed89af5bd285538229fd447bec225459025
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-13 18:03:51 +0100

    Actualización automática con IA

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuienesSomosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/quienes-somos', [QuienesSomosController::class, 'index'])->name('quienes.somos');
