7bb8d439b609b1cbdcf9cab8d36cbf73b06e9a85
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-14 18:33:28 +0100

    Actualización automática con IA

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuienesSomosController;
+use App\Http\Controllers\JoviController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/quienes-somos', [QuienesSomosController::class, 'index'])->name('quienes.somos');
+
+Route::get('/jovi', [JoviController::class, 'index'])->name('jovi.index');
